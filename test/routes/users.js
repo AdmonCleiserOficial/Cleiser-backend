d0fc7cdda35e81cc363e69d6796daa3eb40b9603
Kingsley Victor <javaprodigy56@gmail.com>
2019-01-21 15:57:03 +0100

    Provided tests for a few endpoints

--- test/routes/users.js
+++ test/routes/users.js
@@ -1,10 +1,53 @@
-describe('ROUTE: Users', () => {
-    const User = require('../../models/user');
-    beforeEach(done => {
-        User.deleteMany({})
-        .then(r => User.create({
+const User = require('../../models/user');
+let token;
 
-        }))
-        .catch(err => done(err));
+describe('ROUTE: Users', () => {
+    describe('POST:', () => {
+        beforeEach(done => {
+            User.deleteMany({})
+            .then(() => User.create({
+                email: 'jane@gmail.com',
+                password: 'janeth'
+            }))
+            .then(() => done());
+        });
+        it('should create a user', done => {
+            request.post('/users/register')
+            .send({
+                email: 'jackson@gmail.com',
+                password: 'jackyjacky'
+            })
+            .expect(200)
+            .end((err, res) => {
+                expect(res.body).to.include.keys("token", "user");
+                done(err);
+            });
+        });
+        it('should generate an auth token', done => {
+            request.post('/users/authenticate')
+            .send({
+                email: 'jane@gmail.com',
+                password: 'janeth'
+            })
+            .expect(200)
+            .end((err, res) => {
+                expect(res.body).to.include.keys("token");
+                console.log(res.body);
+                token = res.body.token;
+                done(err);
+            });
+        });
+    });
+    describe('POST:', () => {
+        it('should extract token and return user', done => {
+            request.get('/users/login')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
+            .end((err, res) => {
+                expect(res.body.success).to.be.true;
+                console.log(res.body);
+                done(err);
+            });
+        });
     });
 });
